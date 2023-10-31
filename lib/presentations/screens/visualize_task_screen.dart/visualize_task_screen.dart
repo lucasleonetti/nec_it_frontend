6c95e352e105de53c94a31b7d3f77d01ce8e313f
Lucas Leonetti <lucasleone95@gmail.com>
2023-10-30 21:55:11 -0300

    create visualize screen

---
+++ lib/presentations/screens/visualize_task_screen.dart/visualize_task_screen.dart
@@ -0,0 +1,116 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import '../../../config/theme/app_theme.dart';
+
+class VisualizeTaskScreen extends StatelessWidget {
+  const VisualizeTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Tareas'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  ScaffoldState().openDrawer();
+                },
+                icon: const Icon(Icons.notifications))
+          ],
+        ),
+        body: const _ColaboratorDashboard(),
+        bottomNavigationBar: const _BottomNavigatorBar());
+  }
+}
+
+class _BottomNavigatorBar extends StatelessWidget {
+  const _BottomNavigatorBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Wrap(
+        spacing: 30,
+        alignment: WrapAlignment.center,
+        children: [
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.assistant_rounded),
+              label: const Text("Solicitar Asistencia")),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: SizedBox(
+              width: double.infinity,
+              child: Text("NEC IT",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: AppTheme().getTheme().primaryColor)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _ColaboratorDashboard extends StatelessWidget {
+  const _ColaboratorDashboard();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FadeInLeft(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Pendiente',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  Text('Completada',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.red.shade200,
+                        ),
+                        child: const Center(
+                          child: Text('Pending tasks'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.green.shade200,
+                        ),
+                        child: const Center(
+                          child: Text('Completed tasks'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
