5649f8eeffb3ff40918e5808d9fab44d0d32961a
Lucas Leonetti <lucasleone95@gmail.com>
2023-10-30 21:55:29 -0300

    create assign task screen

--- lib/presentations/screens/assign_task_screen/assign_task_screen.dart
+++ lib/presentations/screens/assign_task_screen/assign_task_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AssignTaskScreen extends StatelessWidget {
-const AssignTaskScreen({ Key? key }) : super(key: key);
+  const AssignTaskScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
