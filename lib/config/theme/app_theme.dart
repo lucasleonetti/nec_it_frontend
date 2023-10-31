af95cf7b792ff70635320ddf60e493a6eba98610
Lucas Leonetti <lucasleone95@gmail.com>
2023-10-30 21:54:49 -0300

    update app theme parameters

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,5 +3,25 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        iconTheme: const IconThemeData(color: Colors.blueGrey),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.blueGrey,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueGrey,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        shadowColor: Colors.blueGrey,
+        primaryIconTheme: const IconThemeData(color: Colors.blueGrey),
       );
 }
