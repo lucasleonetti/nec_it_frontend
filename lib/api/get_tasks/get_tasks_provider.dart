cc2a085a69f39443e9223cf4644f7131d1dff0ff
Lucas Leonetti <lucasleone95@gmail.com>
2023-11-07 12:57:56 -0300

    conected api tasks for dashboard tasks

--- lib/api/get_tasks/get_tasks_provider.dart
+++ lib/api/get_tasks/get_tasks_provider.dart
@@ -1,6 +1,5 @@
 import 'package:nec_it_frontend/api/get_tasks/TaskInformation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
 import '../../model/task_model/task_model.dart';
 part 'get_tasks_provider.g.dart';
 
--- lib/presentations/screens/visualize_task_screen.dart/visualize_task_screen.dart
+++ lib/presentations/screens/visualize_task_screen.dart/visualize_task_screen.dart
@@ -1,5 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nec_it_frontend/api/get_tasks/get_tasks_provider.dart';
 import '../../../config/theme/app_theme.dart';
 
 class VisualizeTaskScreen extends StatelessWidget {
@@ -21,6 +23,116 @@ class VisualizeTaskScreen extends StatelessWidget {
         ),
         body: const _ColaboratorDashboard(),
         bottomNavigationBar: const _BottomNavigatorBar());
+  }
+}
+
+class _ColaboratorDashboard extends ConsumerWidget {
+  const _ColaboratorDashboard();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tasksProvider = ref.watch(getTasksProvider);
+
+    final pendingTasks = tasksProvider.whenData(
+        (task) => task.where((task) => task.estado == 'Pendiente').toList());
+
+    final completedTasks = tasksProvider.whenData(
+        (task) => task.where((task) => task.estado == 'Completada').toList());
+
+    return Scaffold(
+      body: FadeInLeft(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Pendiente',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  Text('Completada',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  children: [
+                    // Muestra las tareas pendientes
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.red.shade200,
+                        ),
+                        child: pendingTasks.when(
+                          data: (tasks) => ListView.separated(
+                            itemCount: tasks.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  '${tasks[index].descripcion}: ${tasks[index].destino}',
+                                  softWrap: true,
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                                leading: const Icon(Icons.pending_actions),
+                                enableFeedback: true,
+                                // otros detalles de la tarea...
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                Divider(color: Colors.blueGrey, endIndent: 12),
+                          ),
+                          loading: () =>
+                              const Center(child: CircularProgressIndicator()),
+                          error: (err, stack) =>
+                              const Center(child: Text('Ha ocurrido un error')),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    // Muestra las tareas completadas
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.green.shade200,
+                        ),
+                        child: completedTasks.when(
+                          data: (tasks) => ListView.separated(
+                            itemCount: tasks.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  '${tasks[index].descripcion}: ${tasks[index].destino}',
+                                  softWrap: true,
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                                leading: const Icon(
+                                    Icons.check_circle_outline_outlined),
+                                enableFeedback: true,
+                                // otros detalles de la tarea...
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                Divider(color: Colors.blueGrey, endIndent: 12),
+                          ),
+                          loading: () =>
+                              const Center(child: CircularProgressIndicator()),
+                          error: (err, stack) =>
+                              const Center(child: Text('Ha ocurrido un error')),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -51,65 +163,6 @@ class _BottomNavigatorBar extends StatelessWidget {
             ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class _ColaboratorDashboard extends StatelessWidget {
-  const _ColaboratorDashboard();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FadeInLeft(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text('Pendiente',
-                      style: TextStyle(fontWeight: FontWeight.w500)),
-                  Text('Completada',
-                      style: TextStyle(fontWeight: FontWeight.w500)),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.red.shade200,
-                        ),
-                        child: const Center(
-                          child: Text('Pending tasks'),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.green.shade200,
-                        ),
-                        child: const Center(
-                          child: Text('Completed tasks'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
