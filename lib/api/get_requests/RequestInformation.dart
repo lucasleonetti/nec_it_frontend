fdf5f9c8222ac30754bcbe49988e2954142dc945
Lucas Leonetti <lucasleone95@gmail.com>
2023-11-07 18:47:57 -0300

    refactor endpoint api

--- lib/api/get_requests/RequestInformation.dart
+++ lib/api/get_requests/RequestInformation.dart
@@ -7,8 +7,9 @@ class RequestInformation {
 
     await Future.delayed(const Duration(seconds: 1));
 
-    final response = await dio.get('http://192.168.0.124:3000/solicitudes');
-    return (response.data['solicitudes'] as List)
+    final response =
+        await dio.get('http://192.168.0.124:3000/solicitudes_recibidas');
+    return (response.data['solicitudes_recibidas'] as List)
         .map((e) => Solicitud.fromJson(e))
         .toList();
   }
